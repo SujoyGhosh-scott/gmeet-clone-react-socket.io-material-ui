2f0ed9cb6e7aa3fcd8e9b27ed149d7e9b88321a9
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2021-06-13 18:15:15 +0530

    boiler plate

---
+++ client/src/components/NewRoomButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewRoomButton() {
+  return (
+    <div>
+      <h1>new room button</h1>
+    </div>
+  );
+}
+
+export default NewRoomButton;
---
+++ client/src/components/Room.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Room() {
+  return (
+    <div>
+      <h1>Room</h1>
+    </div>
+  );
+}
+
+export default Room;
